a0b3c2372de6f9447e9e041330df79582f1ff723
guchi5 <zero19390901@gmail.com>
2024-02-26 11:17:14 +0900

    プログラムの修正 - DatabaseManagerクラスの追加 - SQLインジェクション対策 - DBへのアクセスを一度に制限

--- app.rb
+++ app.rb
@@ -5,41 +5,48 @@ require 'sinatra/reloader'
 require 'json'
 require 'pg'
 
+class DatabaseManager
+  def initialize
+    @conn = PG.connect(dbname: 'postgres')
+    result = @conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+    @conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)') if result.values.empty?
+  end
+
+  def test
+    p "hello world!!\n"
+  end
+
+  def read_memos
+    @conn.exec('SELECT * FROM memos')
+  end
+
+  def read_memo_by_id(id)
+    @conn.exec('SELECT * FROM memos WHERE id = $1', [id])
+  end
+
+  def add_memo(memo)
+    @conn.exec('INSERT INTO memos (title, content) VALUES ($1, $2)', [memo[:title], memo[:content]])
+  end
+
+  def update_memo(memo)
+    @conn.exec('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [memo[:title], memo[:content], memo[:id]])
+  end
+
+  def delete_memo(id)
+    @conn.exec('DELETE FROM memos WHERE id = $1', [id])
+  end
+end
+
+db = DatabaseManager.new
+
 helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
-
-  def read_memos
-    conn = PG.connect(dbname: 'postgres')
-    result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
-    conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)') if result.values.empty?
-    conn.exec('SELECT * FROM memos')
-  end
-
-  def read_memo_by_id(id)
-    conn = PG.connect(dbname: 'postgres')
-    conn.exec("SELECT * FROM memos WHERE id = #{id}")
-  end
-
-  def add_memo(memo)
-    conn = PG.connect(dbname: 'postgres')
-    conn.exec("INSERT INTO memos (title, content) VALUES ('#{memo[:title]}', '#{memo[:text]}')")
-  end
-
-  def update_memo(memo)
-    conn = PG.connect(dbname: 'postgres')
-    conn.exec("UPDATE memos SET title = '#{memo[:title]}', content = '#{memo[:text]}' WHERE id = #{memo[:id]}")
-  end
-
-  def delete_memo(id)
-    conn = PG.connect(dbname: 'postgres')
-    conn.exec("DELETE FROM memos WHERE id = #{id}")
-  end
 end
 
 get '/memos' do
-  @memos = read_memos
+  @memos = db.read_memos
   erb :index
 end
 
@@ -53,36 +60,36 @@ end
 
 post '/memos' do
   @title = params[:title]
-  @text = params[:text]
-  memo = { title: @title, text: @text }
-  add_memo(memo)
+  @content = params[:content]
+  memo = { title: @title, content: @content }
+  db.add_memo(memo)
   redirect '/'
 end
 
 get '/memos/:id/edit' do |id|
-  memo = read_memo_by_id(id)
+  memo = db.read_memo_by_id(id)
   @title = memo[0]['title']
-  @text = memo[0]['content']
+  @content = memo[0]['content']
   @id = id
   erb :edit_memo
 end
 
 patch '/memos/:id' do |id|
   title = params[:title]
-  text = params[:text]
-  update_memo({ id:, title:, text: })
+  content = params[:content]
+  db.update_memo({ id:, title:, content: })
   redirect '/'
 end
 
 get '/memos/:id' do |id|
-  memo = read_memo_by_id(id)
+  memo = db.read_memo_by_id(id)
   @title = memo[0]['title']
-  @text = memo[0]['content']
+  @content = memo[0]['content']
   @id = id
   erb :show_memo
 end
 
 delete '/memos/:id' do |id|
-  delete_memo(id)
+  db.delete_memo(id)
   redirect '/'
 end
